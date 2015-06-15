d834a0aae3ec9c3e919e883a7f28a3f4c93feca0
khromov <stanislav.khromov@gmail.com>
2015-06-15 23:42:13 +0200

    Initial

---
+++ obscure-captcha.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Plugin Name: Obscure CAPTCHA
+Plugin URI:
+Description: Shh, it's a secret!
+Version: 1.0
+Author: khromov
+Author URI: http://profiles.wordpress.org/khromov/
+License: GPL2
+*/
+include 'obscure-functions.php';
+
+/**
+ * This will let us set a question prefix and register our questions.
+ * These arent hooked anywhere because we want them to run as early as possible.
+ */
+add_filter('signup_captcha_questions_prefix', function($prefix)
+{
+	return __('Kontrollfråga: ', 'user_registration_captcha') . '<br/>';
+});
+
+add_filter('signup_captcha_questions', function($questions)
+{
+	$questions['Vilken färg har himlen?'] = 'blå';
+	$questions['Vilken färg har sveriges flagga, förutom blå?'] = 'gul';
+
+	return $questions;
+});
+
+
+add_action('plugins_loaded', function()
+{
+	$question_prefix = apply_filters('signup_captcha_questions_prefix', '');
+	$questions = apply_filters('signup_captcha_questions', array());
+
+	/**
+	 * Add question in single sites
+	 */
+	add_action('register_form', function() use ($question_prefix, $questions)
+	{
+		oc_user_registration_captcha_render_form($question_prefix, array_rand($questions));
+		?>
+	<?php
+	}, 100);
+
+	/**
+	 * Hook into registration on single sites and check answer
+	 */
+	add_action('register_post', function($login, $email, $errors)
+	{
+		//FIXME: CHECK IF THIS JUST WORKS or if we need check like in wpmu_validate_user_signup
+
+		/* Here we will set defaults for all the values we need */
+		$postdata = oc_default_registration_postdata($_POST);
+
+		/* @var $errors WP_Error */
+		/** Check if the control value was empty and error out if it was **/
+
+		/* If users magically filled out the hidden field, boot them */
+		if($postdata['signup_captcha_hp'] !== '')
+			$errors->add('signup_captcha_bot_error', __('<strong>FEL</strong>: Du råkade fylla i ett osynligt fält! Säker på att du inte är en robot?'));
+
+		/* Grab questions here via the same filter as we use for building the form */
+		$questions = apply_filters('signup_captcha_questions', array());
+
+		/* Check if what was submitted is a proper question and in such case, grab the answer */
+		$answer = (isset($questions[base64_decode($postdata['signup_captcha_question'])])) ? $questions[base64_decode($postdata['signup_captcha_question'])] : null;
+
+		/* If not a valid question or the answer is incorrect, add an error */
+		if($answer === NULL || strtolower($postdata['signup_captcha_answer']) !== strtolower($answer))
+			$errors->add('signup_captcha_wrong_answer',__('<strong>FEL</strong>: Svaret på kontrollfrågan var felaktigt.'));
+
+	}, 10, 3);
+
+
+	/**
+	 * Multisite registration
+	 */
+	if(is_multisite())
+	{
+		/**
+		 * Ask question
+		 */
+		add_action('signup_extra_fields', function($errors) use ($question_prefix, $questions)
+		{
+			$errors_new = array();
+			$errors_new[] = $errors->get_error_message('signup_captcha_bot_error');
+			$errors_new[] = $errors->get_error_message('signup_captcha_wrong_answer');
+
+			oc_user_registration_captcha_render_form($question_prefix, array_rand($questions), $errors_new);
+		});
+
+		/**
+		 * Check answer
+		 */
+		add_filter('wpmu_validate_user_signup', function($content)
+		{
+			/* Don't do the validation stuff if we're adding a user from the admin page and user has the permission to do it */
+			if(is_admin() && current_user_can('create_users'))
+				return $content;
+
+			/* Here we will set defaults for all the values we need */
+			$postdata = oc_default_registration_postdata($_POST);
+
+			/* @var $content['errors'] WP_Error */
+			/** Check if the control value was empty and error out if it was **/
+
+			/* If users magically filled out the hidden field, boot them */
+			if($postdata['signup_captcha_hp'] !== '')
+				$content['errors']->add('signup_captcha_bot_error', __('<strong>FEL</strong>: Du råkade fylla i ett osynligt fält! Säker på att du inte är en robot?'));
+
+			/* Grab questions here via the same filter as we use for building the form */
+			$questions = apply_filters('signup_captcha_questions', array());
+
+			/* Check if what was submitted is a proper question and in such case, grab the answer */
+			$answer = (isset($questions[base64_decode($postdata['signup_captcha_question'])])) ? $questions[base64_decode($postdata['signup_captcha_question'])] : null;
+
+			/* If not a valid question or the answer is incorrect, add an error */
+			if($answer === NULL || strtolower($postdata['signup_captcha_answer']) !== strtolower($answer))
+				$content['errors']->add('signup_captcha_wrong_answer',__('<strong>FEL</strong>: Svaret på kontrollfrågan var felaktigt.'));
+
+			return $content;
+		});
+	}
+
+}, 11);
---
+++ obscure-functions.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Renders the registration CAPTCHA form
+ *
+* @param $question_prefix
+* @param $question_html_entities
+* @param $question
+* @param null $errors
+* @param string $answer
+ */
+function oc_user_registration_captcha_render_form($question_prefix, $question, $errors = null, $answer = '')
+{
+	?>
+	<p>
+		<!-- Print errors if needed -->
+		<?php
+			if(is_array($errors))
+			{
+				foreach($errors as $error)
+					echo ($error) ? "<p class=\"error\">{$error}</p>" : '';
+			}
+		?>
+		<label>
+			<span>
+				<strong>
+					<?=$question_prefix?>
+				</strong>
+				<?=oc_convert_to_htmlentities($question)?>
+			</span>
+			<br/>
+			<input type="text" name="signup_captcha_answer" value="<?=$answer?>" class="input" value="" size="20" tabindex="1000"><br/>
+			<input type="hidden" name="signup_captcha_question" value="<?=base64_encode($question)?>">
+			<input type="hidden" name="signup_captcha_check" value="">
+		</label>
+
+		<label id="signup_captcha_id">HP<br/>
+			<input type="text" name="signup_captcha_hp" value="" class="input" size="20" tabindex="1001" />
+		</label>
+
+		<script>
+			document.getElementById("signup_captcha_id").style.display = "none";
+		</script>
+
+		<!-- Technically necessary? Bah, who knows... -->
+		<noscript>
+			<style type="text/css">
+				#signup_captcha_id
+				{
+					display: none;
+				}
+			</style>
+		</noscript>
+	</p>
+	<?php
+}
+
+/**
+ * Helper function to convert string to proper htmlentities (via UTF-32 detour)
+ *
+ * @param $in
+ * @return string
+ */
+function oc_convert_to_htmlentities($in) {
+	/* Convert all characters to HTML entities */
+	if(function_exists('mb_convert_encoding')) {
+		$text = mb_convert_encoding($in , 'UTF-32', 'UTF-8');
+		$question_html_entities = unpack("N*", $text);
+		$question_html_entities = array_map(function($n)
+		{
+			return "&#$n;";
+		}, $question_html_entities);
+	}
+	else {
+		$question_html_entities = htmlentities($in); //Not as good...
+	}
+
+	return implode("", $question_html_entities);
+}
+
+/**
+ * Helper function to return default keys in postdata.
+ *
+ * @param $incoming_postdata - $_POST values.
+ * @return array
+ */
+function oc_default_registration_postdata($incoming_postdata) {
+		return array_merge(array(
+			'signup_captcha_answer' => '',
+			'signup_captcha_question' => '',
+			'signup_captcha_check' => '',
+			'signup_captcha_hp' => ''
+	), $incoming_postdata);
+}
